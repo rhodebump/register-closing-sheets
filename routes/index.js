8717e11a6cb07d11e5bf900ad59f9e5e3673be10
ubuntu <rhodebump@gmail.com>
2014-08-16 18:58:09 +0200

    syntax error

--- routes/index.js
+++ routes/index.js
@@ -62,98 +62,95 @@ router.post('/togglehidden', isLoggedIn, function (req, res) {
 });
 router.post('/savesheet', isLoggedIn, function (req, res) {
 
-        // Set our internal DB variable
-        var db = req.db;
+    // Set our internal DB variable
+    var db = req.db;
 
-        // Get our form values. These rely on the "name" attributes
-        //var daysheetdate = req.body.daysheetdate;
-        // Set our collection
-        var collection = db.get('daysheetcollection');
+    // Get our form values. These rely on the "name" attributes
+    //var daysheetdate = req.body.daysheetdate;
+    // Set our collection
+    var collection = db.get('daysheetcollection');
 
-        var daysheet = {
-            "hidden": req.body.hidden,
-            "submit_daysheet": req.body.submit_daysheet,
-            "store": req.body.store,
-            "openername": req.body.openername,
-            "closername": req.body.closername,
-            "processdate": req.body.processdate,
-            "open_1cent": req.body.open_1cent,
-            "open_5cents": req.body.open_5cents,
-            "open_10cents": req.body.open_10cents,
-            "open_25cents": req.body.open_25cents,
-            "open_1dollar": req.body.open_1dollar,
-            "open_5dollars": req.body.open_5dollars,
-            "open_10dollars": req.body.open_10dollars,
-            "open_20dollars": req.body.open_20dollars,
-            "open_50dollars": req.body.open_50dollars,
-            "open_100dollars": req.body.open_100dollars,
-            "open_total": req.body.open_total,
-            "close_1cent": req.body.close_1cent,
-            "close_5cents": req.body.close_5cents,
-            "close_10cents": req.body.close_10cents,
-            "close_25cents": req.body.close_25cents,
-            "close_1dollar": req.body.close_1dollar,
-            "close_5dollars": req.body.close_5dollars,
-            "close_10dollars": req.body.close_10dollars,
-            "close_20dollars": req.body.close_20dollars,
-            "close_50dollars": req.body.close_50dollars,
-            "close_100dollars": req.body.close_100dollars,
-            "close_total": req.body.close_total,
-            "income_cash_store": req.body.income_cash_store,
-            "income_debits": req.body.income_debits,
-            "income_checks": req.body.income_checks,
-            "income_credit_card": req.body.income_credit_card,
-            "income_coupon": req.body.income_coupon,
-            "income_receipts": req.body.income_receipts,
-            "income_gift_certificate_redeemed": req.body.income_gift_certificate_redeemed,
-            "income_deposit_redeemed": req.body.income_deposit_redeemed,
-            "income_total": req.body.income_total,
-            "totala": req.body.totala,
-            "totalb": req.body.totalb,
-            "difference": req.body.difference,
-            "income_cash_tips": req.body.income_cash_tips,
-            "bisque_sales": req.body.bisque_sales,
-            "paint_time": req.body.paint_time,
-            "child_party": req.body.child_party,
-            "adult_party": req.body.adult_party,
-            "party_deposit": req.body.party_deposit,
-            "gift_certificates_sold": req.body.gift_certificates_sold,
-            "credit_card_tips": req.body.credit_card_tips,
-            "sub_total_sales": req.body.sub_total_sales,
-            "sales_tax": req.body.sales_tax,
-            "gross_sales": req.body.gross_sales
+    var daysheet = {
+        "hidden": req.body.hidden,
+        "submit_daysheet": req.body.submit_daysheet,
+        "store": req.body.store,
+        "openername": req.body.openername,
+        "closername": req.body.closername,
+        "processdate": req.body.processdate,
+        "open_1cent": req.body.open_1cent,
+        "open_5cents": req.body.open_5cents,
+        "open_10cents": req.body.open_10cents,
+        "open_25cents": req.body.open_25cents,
+        "open_1dollar": req.body.open_1dollar,
+        "open_5dollars": req.body.open_5dollars,
+        "open_10dollars": req.body.open_10dollars,
+        "open_20dollars": req.body.open_20dollars,
+        "open_50dollars": req.body.open_50dollars,
+        "open_100dollars": req.body.open_100dollars,
+        "open_total": req.body.open_total,
+        "close_1cent": req.body.close_1cent,
+        "close_5cents": req.body.close_5cents,
+        "close_10cents": req.body.close_10cents,
+        "close_25cents": req.body.close_25cents,
+        "close_1dollar": req.body.close_1dollar,
+        "close_5dollars": req.body.close_5dollars,
+        "close_10dollars": req.body.close_10dollars,
+        "close_20dollars": req.body.close_20dollars,
+        "close_50dollars": req.body.close_50dollars,
+        "close_100dollars": req.body.close_100dollars,
+        "close_total": req.body.close_total,
+        "income_cash_store": req.body.income_cash_store,
+        "income_debits": req.body.income_debits,
+        "income_checks": req.body.income_checks,
+        "income_credit_card": req.body.income_credit_card,
+        "income_coupon": req.body.income_coupon,
+        "income_receipts": req.body.income_receipts,
+        "income_gift_certificate_redeemed": req.body.income_gift_certificate_redeemed,
+        "income_deposit_redeemed": req.body.income_deposit_redeemed,
+        "income_total": req.body.income_total,
+        "totala": req.body.totala,
+        "totalb": req.body.totalb,
+        "difference": req.body.difference,
+        "income_cash_tips": req.body.income_cash_tips,
+        "bisque_sales": req.body.bisque_sales,
+        "paint_time": req.body.paint_time,
+        "child_party": req.body.child_party,
+        "adult_party": req.body.adult_party,
+        "party_deposit": req.body.party_deposit,
+        "gift_certificates_sold": req.body.gift_certificates_sold,
+        "credit_card_tips": req.body.credit_card_tips,
+        "sub_total_sales": req.body.sub_total_sales,
+        "sales_tax": req.body.sales_tax,
+        "gross_sales": req.body.gross_sales
 
-        };
-        // Submit to the DB
-        console.log(daysheet);
-        console.log(req.body._id);
+    };
+    // Submit to the DB
+    console.log(daysheet);
+    console.log(req.body._id);
 
-        if (req.body._id) {
-            console.log("doing update");
-            collection.findOne({
-                    _id: req.body._id
-                }, function (err, existingDaysheet) {
-                    console.log("found one");
-                    console.log("submit_daysheet=" + existingDaysheet.submit_daysheet);
+    if (req.body._id) {
+        console.log("doing update");
+        collection.findOne({
+            _id: req.body._id
+        }, function (err, existingDaysheet) {
+            console.log("found one");
+            console.log("submit_daysheet=" + existingDaysheet.submit_daysheet);
 
-                    if (is_editable(req, res, existingDaysheet)) {
-                        updateDB(collection, req, res, daysheet);
-                        sendEmail(req, daysheet);
-                        daysheetDest(req, res, daysheet);
-                    } else {
+            if (is_editable(req, res, existingDaysheet)) {
+                updateDB(collection, req, res, daysheet);
+                sendEmail(req, daysheet);
+                daysheetDest(req, res, daysheet);
+            } else {
 
-                        console.log("cannot save daysheet");
-                        //res.send("Can not save daysheet that was previously submitted");
-                        req.flash('error', 'Can not save daysheet that was previously submitted');
-                        res.location("daysheet");
-                        res.redirect("daysheet?id=" + doc._id);
-
-
-                    }
+                console.log("cannot save daysheet");
+                //res.send("Can not save daysheet that was previously submitted");
+                req.flash('error', 'Can not save daysheet that was previously submitted');
+                res.location("daysheet");
+                res.redirect("daysheet?id=" + doc._id);
 
 
-                }
-            });
+            }
+        });
     } else {
         console.log("doing insert");
         collection.insert(daysheet, function (err, doc) {
